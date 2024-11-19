1aef519173e4351d4bdabd56efef3d3a1fa25d11
gabriel140282 <gkaakmor@myuax.com>
2024-11-19 11:24:19 +0100

    V0.1

---
+++ Estructura_Estudiante.cpp
@@ -0,0 +1,14 @@
+#include "Estructura_Estudiante.h"
+
+using namespace std;
+
+void recogerInformacion() {
+
+    Estudiante estudiante1;
+
+    estudiante1.setNombre("Gabriel");
+    estudiante1.setEdad(20);
+    estudiante1.setPromedio(9.5);
+
+    estudiante1.mostrarEstudiante();
+}
